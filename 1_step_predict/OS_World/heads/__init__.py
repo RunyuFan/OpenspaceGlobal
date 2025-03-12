7102671a2e0c2d58baa091dd7499b19ee1ff2ed4
RunyuFan (樊润宇) <94748663+RunyuFan@users.noreply.github.com>
2025-03-12 13:00:06 +0800

    Update

--- 1_step_predict/OS_World/heads/__init__.py
+++
@@ -1,9 +0,0 @@
-from .upernet import UPerHead
-from .segformer import SegFormerHead
-from .sfnet import SFHead
-from .fpn import FPNHead
-from .fapn import FaPNHead
-# from .SBAHead import TinySBAHead
-# from .SBAHead import MTSBAHead
-from .ContextBlock2d import ContextBlock2d
-__all__ = ['UPerHead', 'SegFormerHead', 'SFHead', 'FPNHead', 'FaPNHead', 'ContextBlock2d']
